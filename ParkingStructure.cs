180615772205814563a69d2ece7d22d02de9e6ae
sudipta <sudsen@microsoft.com>
2017-08-26 15:05:50 +0530

    Version 1

---
+++ ParkingStructure.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingStructure
+{
+    public abstract class Vehicle
+    {
+        public Vehicle(string vehiclenumber)
+        {
+            this.vehiclenumber = vehiclenumber;
+        }
+        public abstract int pay();
+        public DateTime parkedtime;
+        public string vehiclenumber;
+    }
+    public class Motorcycle : Vehicle
+    {
+        public Motorcycle(string vehiclenumber) : base(vehiclenumber)
+        {
+        }
+
+        public override int pay()
+        {
+            if ((DateTime.Now - parkedtime).TotalHours < 3)
+                return 40;
+            else
+                return 60;
+        }
+    }
+    public class Car : Vehicle
+    {
+        public Car(string vehiclenumber) : base(vehiclenumber)
+        {
+        }
+
+        public override int pay()
+        {
+            if ((DateTime.Now - parkedtime).TotalHours < 3)
+                return 60;
+            else
+                return 100;
+        }
+    }
+    public abstract class ParkingSpace
+    {
+
+        private bool isOccupied { get; set; }
+        private int slotNumber;
+        private Vehicle vehicle;
+
+
+        public ParkingSpace(int slotNumber)
+        {
+            isOccupied = false;
+            this.slotNumber = slotNumber;
+        }
+
+        void park()
+        {
+            isOccupied = true;
+        }
+
+        void unPark()
+        {
+            isOccupied = false;
+        }
+
+    
+        public  string GetTicketNumber()
+        {
+            var uniquenumber = Guid.NewGuid().ToString();
+            return uniquenumber;
+        }
+
+        public class SmallSlot : ParkingSpace
+        {
+
+            public SmallSlot(int slotNumber) : base(slotNumber)
+            { 
+            }
+        }
+        public class LargeSlot : ParkingSpace
+        {
+
+            public LargeSlot(int slotNumber) : base(slotNumber)
+            {
+            }
+        }
+        public class ParkingLot
+        {
+
+            private static  int NUMBER_OF_SMALL_SLOTS;
+        
+            private static  int NUMBER_OF_LARGE_SLOTS ;
+            public Dictionary<string, ParkingSpace> occupiedSlots;
+            private List<ParkingSpace> smallSlots;
+          
+            private List<ParkingSpace> largeSlots;
+
+            public ParkingLot(int numofsmallslots,int numoflargeslots)
+            {
+                NUMBER_OF_SMALL_SLOTS = numofsmallslots;
+                NUMBER_OF_LARGE_SLOTS = numoflargeslots;
+                smallSlots = new List<ParkingSpace>(NUMBER_OF_SMALL_SLOTS);
+                largeSlots = new List<ParkingSpace>(NUMBER_OF_LARGE_SLOTS);
+                createSlots();
+                occupiedSlots = new Dictionary<string, ParkingSpace>();
+            }
+
+            private void createSlots()
+            {
+
+                for (int i = 1; i <= NUMBER_OF_SMALL_SLOTS; i++)
+                {
+                    smallSlots.Add(new SmallSlot(i));
+                }
+               
+                for (int i = 1; i <= NUMBER_OF_LARGE_SLOTS; i++)
+                {
+                    largeSlots.Add(new LargeSlot(i));
+                }
+
+            }
+
+            public string park(Vehicle vehicle)
+            {
+
+                ParkingSpace slot;
+                string uniqueToken = string.Empty;
+                switch(vehicle.GetType().Name)
+                {
+                    case "Motorcycle":
+                        if ((slot = getFirstEmptySlot(smallSlots)) != null)
+                        {
+                            uniqueToken = parkHelper(slot, vehicle);
+                        }
+                        break;
+                    case "Car":
+                        if ((slot = getFirstEmptySlot(largeSlots)) != null)
+                        {
+                            uniqueToken = parkHelper(slot, vehicle);
+                        }
+                        break;
+
+                }
+              
+                return uniqueToken;
+            }
+
+            public void unPark(string ticket)
+            {
+                var slot = occupiedSlots[ticket];
+               
+                slot.unPark();
+                occupiedSlots.Remove(ticket);
+              
+
+            }
+
+            private ParkingSpace getFirstEmptySlot(List<ParkingSpace> slots)
+            {
+ 
+                ParkingSpace emptySlot = null;
+
+                foreach(var slot in slots)
+                {
+                    emptySlot = slot;
+                    if (!emptySlot.isOccupied)
+                    {
+                    
+                        return emptySlot;
+                    }
+                }
+                return null;
+            }
+
+            private string parkHelper(ParkingSpace slot, Vehicle vehicle)
+            {
+                slot.park();
+                string ticketnumber = slot.GetTicketNumber() + "@" + slot.slotNumber;
+                slot.vehicle = vehicle;
+                vehicle.parkedtime = DateTime.Now;
+                occupiedSlots.Add(ticketnumber, slot);
+                return ticketnumber;
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using ParkingStructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ParkingStructure.ParkingSpace;
+
+namespace ConsoleApplication27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberofsmallslots = 3;
+            int numberoflargeslots = 3;
+            Console.WriteLine("Number of slots for motorcycle " + numberofsmallslots);
+            Console.WriteLine("Number of slots for cars " + numberoflargeslots);
+
+            ParkingLot obj = new ParkingLot(numberofsmallslots, numberoflargeslots);
+
+            Vehicle vehicle1 = new Motorcycle("MH1024");
+            var uniquetoken1 = obj.park(vehicle1);
+            if (!string.IsNullOrEmpty(uniquetoken1))
+                Console.WriteLine("Parking Successful for motorcycle " + vehicle1.vehiclenumber + " at Slot : " + uniquetoken1.Split('@')[1]);
+            else
+                Console.WriteLine("Parking unsuccessful for motorcycle. Please wait... " + vehicle1.vehiclenumber);
+
+            Vehicle vehicle2 = new Motorcycle("TN4555");
+            var uniquetoken2 = obj.park(vehicle2);
+            if (!string.IsNullOrEmpty(uniquetoken2))
+                Console.WriteLine("Parking Successful for motorcycle " + vehicle2.vehiclenumber + " at Slot : " + uniquetoken2.Split('@')[1]);
+            else
+                Console.WriteLine("Parking unsuccessful for motorcycle. Please wait... " + vehicle2.vehiclenumber);
+
+            Vehicle vehicle3 = new Motorcycle("HM4444");
+
+            var uniquetoken3 = obj.park(vehicle3);
+            if (!string.IsNullOrEmpty(uniquetoken3))
+                Console.WriteLine("Parking Successful for motorcycle " + vehicle3.vehiclenumber + " at Slot : " + uniquetoken3.Split('@')[1]);
+            else
+                Console.WriteLine("Parking unsuccessful for motorcycle. Please wait... " + vehicle3.vehiclenumber);
+
+            Vehicle vehicle4 = new Motorcycle("MH1025");
+            var uniquetoken4 = obj.park(vehicle4);
+            if (!string.IsNullOrEmpty(uniquetoken4))
+                Console.WriteLine("Parking Successful for motorcycle " + vehicle4.vehiclenumber + " at Slot : " + uniquetoken4.Split('@')[1]);
+            else
+                Console.WriteLine("Parking unsuccessful for motorcycle. Please wait... " + vehicle4.vehiclenumber);
+
+            //Unpark Vehicle 2
+            obj.unPark(uniquetoken2);
+            Console.WriteLine("motorcycle unparked from slot " + uniquetoken2.Split('@')[1]);
+
+            var amount = vehicle2.pay();
+            Console.WriteLine("Pay amount to the ticket collector " + amount);
+
+            //Again try parking Vehicle 4
+
+            uniquetoken4 = obj.park(vehicle4);
+            if (!string.IsNullOrEmpty(uniquetoken4))
+                Console.WriteLine("Parking Successful for motorcycle " + vehicle4.vehiclenumber + " at Slot : " + uniquetoken4.Split('@')[1]);
+            else
+                Console.WriteLine("Parking unsuccessful for motorcycle. Please wait... " + vehicle4.vehiclenumber);
+
+            Vehicle car1 = new Car("KN5555");
+
+            var uniquetoken5 = obj.park(car1);
+            if (!string.IsNullOrEmpty(uniquetoken4))
+                Console.WriteLine("Parking Successful for car " + car1.vehiclenumber + " at Slot : " + uniquetoken5.Split('@')[1]);
+            else
+                Console.WriteLine("Parking unsuccessful for car " + car1.vehiclenumber);
+
+            Console.Read();
+        }
+    }
+}
